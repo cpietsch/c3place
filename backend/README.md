074ac2bbe70658e611ed58348e4cf2027e4d22d3
Paul Vollmer <paul.vollmer@protonmail.com>
2019-12-16 23:20:28 +0100

    minimal backend

--- backend/README.md
+++ backend/README.md
@@ -18,3 +18,9 @@ test the api server
 ```sh
 go test
 ```
+
+send a pixel with curl
+
+```sh
+curl -X POST http://localhost:4000/pixel -H 'Content-Type: application/json' -d '{"r": 0, "g": 255, "b": 0, "x": 100, "y": 100}'
+```
---
+++ backend/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	port = ":4000"
+
+	imageWidth  = 1000
+	imageHeight = 1000
+	upLeft      = image.Point{0, 0}
+	lowRight    = image.Point{imageWidth, imageHeight}
+
+	data []PostPixel
+)
+
+type PostPixel struct {
+	R uint8 `json:"r"`
+	G uint8 `json:"g"`
+	B uint8 `json:"b"`
+	X int   `json:"x"`
+	Y int   `json:"y"`
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// https://yourbasic.org/golang/create-image/
+	r.GET("/", func(c *gin.Context) {
+		img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+		for i := 0; i < len(data); i++ {
+			img.Set(data[i].X, data[i].Y, color.RGBA{data[i].R, data[i].G, data[i].B, 0xff})
+		}
+
+		buf := new(bytes.Buffer)
+		png.Encode(buf, img)
+		c.String(http.StatusOK, buf.String())
+	})
+
+	r.POST("/pixel", func(c *gin.Context) {
+		body := PostPixel{}
+		err := c.Bind(&body)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid post body"})
+			return
+		}
+
+		fmt.Println("==> Write pixel to data", body)
+		data = append(data, body)
+
+		c.JSON(http.StatusCreated, gin.H{"status": "created", "pixel": body})
+	})
+
+	return r
+}
+
+func main() {
+	data = []PostPixel{
+		PostPixel{
+			R: 255,
+			G: 0,
+			B: 0,
+			X: 100,
+			Y: 100,
+		},
+	}
+	fmt.Println("DATA", data)
+
+	r := setupRouter()
+	r.Run(port)
+}
